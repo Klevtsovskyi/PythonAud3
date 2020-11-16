a9ec6ef9a1700252d6afa32088f2b999b7c0cd34
Arsen <avklevtsovskiy@gmail.com>
2020-11-16 13:42:00 +0200

    Aud14

---
+++ Aud14_CM04_1_Para/tcm04_03.py
@@ -0,0 +1,51 @@
+
+
+from threading import Thread
+from queue import Queue
+from time import time, sleep
+import random
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+q = Queue()
+start = time()
+
+
+def inp(n, p_time, fixed=False):
+    for i in range(n):
+        t = p_time if fixed else random.random() * (p_time - 1) + 1
+        sleep(t)
+        message = ("ПОВІДОМЛЕННЯ {}: створено в {:.4f}"
+                   .format(i, time() - start))
+        logging.debug("В чергу додано повідомлення {} через {:.4f} секунд"
+                      .format(message, t))
+        q.put(message)
+
+
+def out(n, p_time, fixed=False):
+    for i in range(n):
+        t = p_time if fixed else random.random() * (p_time - 1) + 1
+        message = q.get()
+        logging.debug("З черги вийнято повідомлення {}, яке буде "
+                      "оброблятись {:.4f} секунд"
+                      .format(message, t))
+        sleep(t)
+        print(message, "оброблене через {:.4f} після старту"
+              .format(time() - start))
+
+
+if __name__ == '__main__':
+    n = 10
+    t1 = 2
+    t2 = 4
+
+    th1 = Thread(target=inp, args=(n, t1))
+    th2 = Thread(target=out, args=(n, t2))
+    th1.start()
+    th2.start()
+
+    th1.join()
+    th2.join()
---
+++ Aud14_CM04_1_Para/tcm04_13.py
@@ -0,0 +1,52 @@
+
+
+from threading import Thread, Lock
+from time import time, sleep
+import random
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+lock = Lock()
+start = time()
+
+
+def train(arrive_time, transit_time, index):
+    print("Поїзд {} прибуває через {:.4f} секунд і проходить "
+          "ділянку за {:.4f} секунд"
+          .format(index, arrive_time, transit_time))
+    sleep(arrive_time)
+    logging.debug("Поїзд {} прибув до ділянки".format(index))
+    # lock.acquire()
+    # lock.locked()
+    with lock:
+        logging.debug("Поїзд {} почав проходити ділянку".format(index))
+        sleep(transit_time)
+    # lock.release()
+    logging.debug("Поїзд {} пройшов ділянку".format(index))
+    print("Поїзд {} пройшов ділянку. Повний час подорожі {:.4f}"
+          .format(index, time() - start))
+
+
+def next_train(t1, t2, t3, t4, index):
+    transit_time = random.random() * (t2 - t1) + t1
+    arrive_time = random.random() * (t4 - t3) + t3
+    th = Thread(target=train, args=(arrive_time, transit_time, index))
+    th.start()
+    return th
+
+
+if __name__ == '__main__':
+    n = 10
+    t1 = 1
+    t2 = 5
+    t3 = 0
+    t4 = 30
+
+    ths = []
+    for i in range(n):
+        ths.append(next_train(t1, t2, t3, t4, i))
+    for i in range(n):
+        ths[i].join()
