3207c5cb48781e121aa5e420d83c80eda7d74faa
Arsen <avklevtsovskiy@gmail.com>
2021-11-01 12:00:03 +0200

    T22

--- T22_Op_System/t22_04/archive_logs.py
+++ T22_Op_System/t22_04/archive_logs.py
@@ -6,20 +6,25 @@ import tarfile
 
 def archive_logs(date):
     log_dir = os.path.join(sys.path[0], "logs")
-    if os.path.exists(log_dir):
-        lst = []
-        for name in os.listdir(log_dir):
-            if name.endswith(".log"):
-                log_time = datetime.datetime.strptime(name,
-                                                      "%Y%m%d_%H%M%S.log")
-                if log_time < date:
-                    lst.append(name)
-        if lst:
-            archive = os.path.join(log_dir, date.strftime("%Y%m%d_%H%M%S") + ".tar.gz")
+    assert os.path.exists(log_dir), (
+        "Для архівування log-файлів потрібно "
+        "створити хоча один такий файл (запустіть програму create_log.py)"
+    )
 
-            with tarfile.open(archive, "w:gz") as tf:
-                for name in lst:
-                    tf.add(os.path.join(log_dir, name), name)
+    lst = []
+    for name in os.listdir(log_dir):
+        if name.endswith(".log"):
+            log_time = datetime.datetime.strptime(name, "%Y%m%d_%H%M%S.log")
+            if log_time < date:
+                lst.append(name)
+    if lst:
+        archive = os.path.join(
+            log_dir, date.strftime("%Y%m%d_%H%M%S") + ".tar.gz"
+        )
+        with tarfile.open(archive, "w:gz") as tf:
+            for name in lst:
+                tf.add(os.path.join(log_dir, name), name)
+                os.remove(os.path.join(log_dir, name))
 
 
 if __name__ == "__main__":
--- T22_Op_System/t22_04/create_log.py
+++ T22_Op_System/t22_04/create_log.py
@@ -4,9 +4,6 @@ import datetime
 
 
 def create_log():
-    with open("input.txt", "r", encoding="utf-8") as inp:
-        text = inp.read()
-
     log_dir = os.path.join(sys.path[0], "logs")
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
@@ -14,7 +11,8 @@ def create_log():
     now = datetime.datetime.now()
     filename = now.strftime("%Y%m%d_%H%M%S.log")
     with open(os.path.join(log_dir, filename), "w", encoding="utf-8") as out:
-        out.write(text)
+        with open("input.txt", "r", encoding="utf-8") as inp:
+            out.write(inp.read())
 
 
 if __name__ == "__main__":
