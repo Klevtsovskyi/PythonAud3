901d4cb9ef6000777fe7dfe8a558777b4e3ed502
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 19:44:29 +0000

    Adding numeric_owner as keyword arguement

--- T22_Op_System/t22_04/unarchive_logs.py
+++ T22_Op_System/t22_04/unarchive_logs.py
@@ -30,7 +30,7 @@ def unarchive_logs(date):
                             if not is_within_directory(path, member_path):
                                 raise Exception("Attempted Path Traversal in Tar File")
                     
-                        tar.extractall(path, members, numeric_owner) 
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
                         
                     
                     safe_extract(tf, log_dir)
